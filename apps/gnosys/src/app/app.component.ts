e0fc8ed9057615f5e5c6c4539de9787e1ff9e945
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-10-01 18:35:21 +0300

    initial commit

---
+++ apps/gnosys/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nocode-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'gnosys';
+}

3449722063482456fbcdd94b674001969b346cc7
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-10-04 10:31:35 +0300

    nx add @ngneat/dialog

--- apps/gnosys/src/app/app.module.ts
+++ apps/gnosys/src/app/app.module.ts
@@ -6,10 +6,20 @@ import { WidgetsModule } from '@nocode/widgets';
 import { SvgIconsModule } from '@ngneat/svg-icon';
 
 import { AppComponent } from './app.component';
+import { DialogModule } from '@ngneat/dialog';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SvgIconsModule.forRoot(), WidgetsModule],
+  imports: [BrowserModule, SvgIconsModule.forRoot(), WidgetsModule, DialogModule.forRoot({
+      sizes: {
+        sm: {
+          width: '300px',
+          height: '250px'
+        },
+        // You can customize the default sizes
+        // ...
+      }
+    })],
   providers: [],
   bootstrap: [AppComponent],
 })

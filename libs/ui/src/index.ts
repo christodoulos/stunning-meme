c26fea18b71b34d8c88d5d3bbb2c27376077c89d
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2021-10-04 10:35:16 +0300

    build widgets lib

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/ui.module';
 export * from './lib/code-toolbar/code-toolbar.component';
+export * from './lib/file-name/file-name.component';
--- libs/widgets/src/index.ts
+++ libs/widgets/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/widgets.module';
+export * from './lib/basic-code-editor/basic-code-editor.component';
